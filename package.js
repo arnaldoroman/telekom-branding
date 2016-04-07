831b921988bdd947edf63084d5536b27a8dd237a
Marcel Fahle <m.fahle@gmail.com>
2016-04-07 22:22:16 +0800

    adds description, publishs to atmosphere

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'marcelfahle:telekom-branding',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'The current Telekom Branding Assets',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/marcelfahle/telekom-branding',
   // By default, Meteor will default to using README.md for documentation.
