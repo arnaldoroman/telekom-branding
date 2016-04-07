42d8417816ef414720a5470eef8f45aae654e639
Marcel Fahle <m.fahle@gmail.com>
2016-04-07 23:06:57 +0800

    adds scss

--- package.js
+++ package.js
@@ -13,6 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.3.1');
   api.use('ecmascript');
+  api.use('fourseven:scss@3.4.1');
   api.mainModule('telekom-branding.js');
   api.addAssets([
     'fonts/telegrotesk-fett.eot',
