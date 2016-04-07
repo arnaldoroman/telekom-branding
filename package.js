e5565b1495db73e91dd8ab284796cd6afad554af
Marcel Fahle <m.fahle@gmail.com>
2016-04-07 22:59:09 +0800

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'marcelfahle:telekom-branding',
-  version: '0.0.2',
+  version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'The current Telekom Branding Assets',
   // URL to the Git repository containing the source code for this package.
